fdf5ac102de44f774c7402272efc2fdbb8c0010b
BSFernando <59699530+BSFernando@users.noreply.github.com>
2020-08-23 17:55:40 -0300

    Update git_ano_total.py

--- nc_tsm_anomalia/git_ano_total.py
+++ nc_tsm_anomalia/git_ano_total.py
@@ -5,12 +5,12 @@ total = list(range(1987, 2018))
 meses = ['jan','fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
 cont = 0
 
-nc_file_0 = np.load('C:/Users/User/Downloads/github/media_jan_total.npy')
+nc_file_0 = np.load('C:/Users/User/media_jan_total.npy')
 nc_file_0 = nc_file_0 * 0
 
 for anos in total:  
     
-    nc_ano = 'C:/Users/User/Downloads/github/media_%s_total.npy' % str(anos)
+    nc_ano = 'C:/Users/User/media_%s_total.npy' % str(anos)
     nc_ano = np.load(nc_ano)
 
     nc_file_0 = nc_ano + nc_file_0
@@ -20,6 +20,6 @@ for anos in total:
 A = nc_file_0 / 31
     
 item = pd.DataFrame(A)
-nome = 'C:/Users/User/Downloads/github/media_anos_total.npy' 
+nome = 'C:/Users/User/media_anos_total.npy' 
 np.save(nome, item)
 
