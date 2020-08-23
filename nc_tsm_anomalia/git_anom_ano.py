5a6de2c22b61de331ab16cfe2f3edbe9d531169a
BSFernando <59699530+BSFernando@users.noreply.github.com>
2020-08-23 17:56:53 -0300

    Update git_anom_ano.py

--- nc_tsm_anomalia/git_anom_ano.py
+++ nc_tsm_anomalia/git_anom_ano.py
@@ -6,18 +6,18 @@ meses = ['jan','fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'n
 
 for anos in total:
     
-    nc_ano = 'C:/Users/User/Downloads/github/media_%s_total.npy' % str(anos)
+    nc_ano = 'C:/Users/User/media_%s_total.npy' % str(anos)
     nc_ano = np.load(nc_ano)
     
-    nc_ano_total = 'C:/Users/User/Downloads/github/media_anos_total.npy' 
+    nc_ano_total = 'C:/Users/User/media_anos_total.npy' 
     nc_ano_total = np.load(nc_ano_total)
 
-    nc_desv = 'C:/Users/User/Downloads/github/desv_anos_total.npy'
+    nc_desv = 'C:/Users/User/desv_anos_total.npy'
     nc_desv = np.load(nc_desv)
 
             
     anom_norm = (nc_ano - nc_ano_total) / nc_desv
                 
     anom_norm = pd.DataFrame(anom_norm)
-    nome = 'C:/Users/User/Downloads/github/anomalia_%s_total.npy' % str(anos)
+    nome = 'C:/Users/User/anomalia_%s_total.npy' % str(anos)
     np.save(nome, anom_norm)
