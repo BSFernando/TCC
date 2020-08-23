73d6cc0013c9cb46eb7196915e85ac87cf86d298
BSFernando <59699530+BSFernando@users.noreply.github.com>
2020-08-23 18:01:18 -0300

    Update git_media_mes_ano.py

--- nc_tsm_anomalia/git_media_mes_ano.py
+++ nc_tsm_anomalia/git_media_mes_ano.py
@@ -6,7 +6,7 @@ anos = list(range(1987, 2018))
 meses = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"]
 lista_meses1 = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
 
-nc_file_0 = 'C:/Users/User/Downloads/tcc/mapas/.nc/sst.day.mean.1987.nc'     
+nc_file_0 = 'C:/Users/User/sst.day.mean.1987.nc'     
 sst_0 = n4.Dataset(nc_file_0)
 sst_0 = sst_0.variables['sst'][0,:,:]
 sst_0 = sst_0 * 0
@@ -18,7 +18,7 @@ while contador < len(meses):
         
     for v in anos:    
         
-        nc_file = 'C:/Users/User/Downloads/tcc/mapas/.nc/sst.day.mean.%s.nc' % str(v)
+        nc_file = 'C:/Users/User/sst.day.mean.%s.nc' % str(v)
         ano_data = n4.Dataset(nc_file)
         cont_dia = lista_meses1[contador]
         cont_dia_1 = 0
@@ -32,7 +32,7 @@ while contador < len(meses):
     
         sstf = sstf/cont_dia_1
         sstf = pd.DataFrame(sstf)
-        np.save(('C:/Users/User/Downloads/github/media_%s_%s.npy' % (str(meses[contador]), str(v))), sstf)
+        np.save(('C:/Users/User/media_%s_%s.npy' % (str(meses[contador]), str(v))), sstf)
     
     contador = contador + 1
     contador1 = contador1 + 1    
