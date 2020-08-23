a5d906c9277f835f4a0bd57c4d448c59e383b134
BSFernando <59699530+BSFernando@users.noreply.github.com>
2020-08-23 17:58:33 -0300

    Update git_desv_sst.py

--- nc_tsm_anomalia/git_desv_sst.py
+++ nc_tsm_anomalia/git_desv_sst.py
@@ -4,14 +4,14 @@ import numpy as np
 total = list(range(1987, 2018))
 meses = ['jan','fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez']
     
-nc_file_0 = np.load('C:/Users/User/Downloads/github/media_jan_total.npy')
+nc_file_0 = np.load('C:/Users/User/media_jan_total.npy')
 nc_file_0 = nc_file_0 * 0
 
 for anos in total:  
     
-    nc_ano = 'C:/Users/User/Downloads/github/media_%s_total.npy' % str(anos)
+    nc_ano = 'C:/Users/User/media_%s_total.npy' % str(anos)
     nc_ano = np.load(nc_ano)
-    nc_ano_media = 'C:/Users/User/Downloads/github/media_anos_total.npy'
+    nc_ano_media = 'C:/Users/User/media_anos_total.npy'
     nc_ano_media = np.load(nc_ano_media)
 
     somatorio = (nc_ano - nc_ano_media) ** 2
@@ -20,6 +20,6 @@ for anos in total:
 E = ((nc_file_0/31) ** (1/2))
     
 item = pd.DataFrame(E)
-nome = 'C:/Users/User/Downloads/github/desv_anos_total.npy' 
+nome = 'C:/Users/User/desv_anos_total.npy' 
 np.save(nome, item)
 
