ccbb3d0c5620b4eb951757d23343fcceed43e455
BSFernando <59699530+BSFernando@users.noreply.github.com>
2020-09-12 19:32:12 -0300

    Update git_anom_mes.py

--- nc_tsm_anomalia/git_anom_mes.py
+++ nc_tsm_anomalia/git_anom_mes.py
@@ -6,22 +6,22 @@ meses = ['jan','fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'n
 
 for mes in meses:
     
-    nc_file_0 = np.load('C:/Users/User/media_jan_total.npy')
+    nc_file_0 = np.load('C:/media_jan_total.npy')
     nc_file_0 = nc_file_0 * 0
     
     for anos in total:
         
-        nc_mes_ano = 'C:/Users/User/media_%s_%s.npy' % (str(mes), str(anos))
+        nc_mes_ano = 'C:/media_%s_%s.npy' % (str(mes), str(anos))
         nc_mes_ano = np.load(nc_mes_ano)
         
-        nc_mes_total = 'C:/Users/User/media_%s_total.npy' % str(mes)
+        nc_mes_total = 'C:/media_%s_total.npy' % str(mes)
         nc_mes_total = np.load(nc_mes_total)
 
-        nc_desv = 'C:/Users/User/desv_%s.npy' % str(mes)
+        nc_desv = 'C:/desv_%s.npy' % str(mes)
         nc_desv = np.load(nc_desv)
     
         anom_norm = (nc_mes_ano - nc_mes_total) / nc_desv
                         
         anom_norm = pd.DataFrame(anom_norm)
-        nome = 'C:/Users/User/anomalia_%s_%s.npy' % (str(mes), str(anos))
+        nome = 'C:/anomalia_%s_%s.npy' % (str(mes), str(anos))
         np.save(nome, anom_norm)
