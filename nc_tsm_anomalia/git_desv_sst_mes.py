74c21a7f63f0156acb73a0084d2ab22dacc0ade2
BSFernando <59699530+BSFernando@users.noreply.github.com>
2020-09-12 19:33:08 -0300

    Update git_desv_sst_mes.py

--- nc_tsm_anomalia/git_desv_sst_mes.py
+++ nc_tsm_anomalia/git_desv_sst_mes.py
@@ -5,14 +5,14 @@ meses = ['jan','fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'n
 
 for mes in meses:
     
-    nc_file_0 = np.load('C:/Users/User/media_jan_total.npy')
+    nc_file_0 = np.load('C:/media_jan_total.npy')
     nc_file_0 = nc_file_0 * 0
     
     for anos in total:  
         
-        nc_mes_ano = 'C:/Users/User/media_%s_%s.npy' % (str(mes), str(anos))
+        nc_mes_ano = 'C:/media_%s_%s.npy' % (str(mes), str(anos))
         nc_mes_ano = np.load(nc_mes_ano)
-        nc_mes_total = 'C:/Users/User/media_%s_total.npy' % str(mes)
+        nc_mes_total = 'C:/media_%s_total.npy' % str(mes)
         nc_mes_total = np.load(nc_mes_total)
 
         somatorio = (nc_mes_ano - nc_mes_total) ** 2
@@ -20,6 +20,6 @@ for mes in meses:
         
     nc_file_0 = ((nc_file_0/31) ** (1/2))
 
-    nome = 'C:/Users/User/desv_%s.npy' % str(mes)
+    nome = 'C:/desv_%s.npy' % str(mes)
     np.save(nome, nc_file_0)
 
