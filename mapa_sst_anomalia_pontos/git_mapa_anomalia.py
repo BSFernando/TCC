a0c24cb82c06cde73ad341c815678dbfc112ff49
BSFernando <59699530+BSFernando@users.noreply.github.com>
2020-09-15 17:23:20 -0300

    Update git_mapa_anomalia.py

--- mapa_sst_anomalia_pontos/git_mapa_anomalia.py
+++ mapa_sst_anomalia_pontos/git_mapa_anomalia.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
-import scipy.cluster.vq as sc
-import os
-os.environ['PROJ_LIB'] = r'C:\Users\User\Anaconda3\pkgs\proj4-5.2.0-ha925a31_1\Library\share'    
+import scipy.cluster.vq as sc   
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 
